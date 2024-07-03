ba370613069144c9fe51e96d8966b02f1b4ac7f4
jh.ko <jeonghunko@gmail.com>
2024-07-03 17:22:33 +0900

    [feature] diff

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -13,3 +13,17 @@ TEST(TestCaseName, CheckLenghSame) {
 	EXPECT_EQ(60, similer.checkLengh("ABCDEF", "GHIJKL"));
 }
 
+TEST(TestCaseName, CheckLenghDiff) {
+	SimilerChecker similer;
+
+	EXPECT_EQ(20, similer.checkLengh("ABCDE", "CDE"));
+	EXPECT_EQ(0, similer.checkLengh("ABCD", "AB"));
+	EXPECT_EQ(0, similer.checkLengh("ABC", "A"));
+	EXPECT_EQ(48, similer.checkLengh("ABCDEF", "ABCDE"));
+
+	EXPECT_EQ(20, similer.checkLengh("CDE", "ABCDE"));
+	EXPECT_EQ(0, similer.checkLengh("AB", "ABCD"));
+	EXPECT_EQ(0, similer.checkLengh("A", "ABC"));
+	EXPECT_EQ(48, similer.checkLengh("ABCDE", "ABCDEF"));
+}
+
--- SimilerTest/similer.cpp
+++ SimilerTest/similer.cpp
@@ -4,10 +4,32 @@ using namespace std;
 
 class SimilerChecker {
 public:
-	int checkLengh(const string& a, const string& b) {
-		if (a.size() == b.size()) {
+	int checkLengh(const string& stringA, const string& stringB) {
+		int result = 0;
+		int sizeA = stringA.size();
+		int sizeB = stringB.size();
+		int min = 0;
+		int gap = 0;
+
+		if (sizeA == sizeB) {
 			return 60;
 		}
-		return 0;
+
+		if (sizeA > sizeB) {
+			if (sizeA >= sizeB*2) return 0;
+
+			min = sizeB;
+			gap = sizeA - sizeB;
+		}
+		else {
+			if (sizeB >= sizeA*2) return 0;
+
+			min = sizeA;
+			gap = sizeB - sizeA;
+		}
+
+		result = (100 - 100 * gap / min) * 60 / 100;
+
+		return result;
 	}
 };
