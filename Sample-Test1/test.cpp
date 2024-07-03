5686baa4dc6f44b781f3529c97a1a9127ef0dbab
jh.ko <jeonghunko@gmail.com>
2024-07-03 17:05:40 +0900

    [feature] same

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,15 @@
 #include "pch.h"
 #include "../SimilerTest/similer.cpp"
+#include <string>
 
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, CheckLenghSame) {
+	SimilerChecker similer;
+
+	EXPECT_EQ(60, similer.checkLengh("ABCDE", "ABCDE"));
+	EXPECT_EQ(60, similer.checkLengh("ABC", "ABC"));
+	EXPECT_EQ(60, similer.checkLengh("ABCDE", "ABABC"));
+	EXPECT_EQ(60, similer.checkLengh("ABCDEF", "GHIJKL"));
 }
+
--- SimilerTest/similer.cpp
+++ SimilerTest/similer.cpp
@@ -1,3 +1,13 @@
-class SimilerChecker {
+#include <string>
 
+using namespace std;
+
+class SimilerChecker {
+public:
+	int checkLengh(const string& a, const string& b) {
+		if (a.size() == b.size()) {
+			return 60;
+		}
+		return 0;
+	}
 };
